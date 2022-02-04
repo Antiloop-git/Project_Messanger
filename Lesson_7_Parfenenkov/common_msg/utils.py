2f7336bd66cf81a6759cd370fcf61e021687cc59
Antiloop-git <969@mail.ru>
2022-02-04 16:08:26 +0300

    lesson_7:

---
+++ Lesson_7_Parfenenkov/common_msg/utils.py
@@ -0,0 +1,44 @@
+"""Утилиты"""
+
+import json, sys, os
+sys.path.append(os.path.join(os.getcwd(), '..'))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from errors import IncorrectDataRecivedError, NonDictInputError
+from decos import Log
+sys.path.append('../')
+
+from common_msg.variables import MAX_PACKAGE_LENGTH, ENCODING
+sys.path.append('..')
+
+@Log()
+def get_message(client):
+    """
+    Утилита приёма и декодирования сообщения принимает байты выдаёт словарь,
+    если приняточто-то другое отдаёт ошибку значения
+    :param client:
+    :return:
+    """
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
+
+
+@Log()
+def send_message(sock, message):
+    """
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
+    """
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
